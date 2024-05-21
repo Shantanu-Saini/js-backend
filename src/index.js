3ce59bb9a6416005bdc044462dd56d0f93825d5d
Shantanu <shantanusaini7000@gmail.com>
2024-05-22 00:38:07 +0530

    connect to mongoDB atlas

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import express from 'express';
+import { DB_NAME } from './constants';
+
+const app = express();
+
+; (async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        app.on("error", (error) => {
+            console.log(error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log("ERR:", error);
+        throw error
+    }
+})()
