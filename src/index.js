04cef2756c83f0a274abbd0ab5c2636704b58190
Shantanu <shantanusaini7000@gmail.com>
2024-05-23 23:13:45 +0530

    setup cors & cookie parser

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import dotenv from 'dotenv'
 import connectDB from "./db/connect.js";
 
 
-dotenv.config({path: './env'})
+dotenv.config({ path: './env' })
 // console.log(process.env);
-connectDB();    
+
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("Server ERR : ", error);
+            throw error
+        })
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    })
+    .catch((err) => {
+        console.log("MONGODB ERR : ", err);
+    })
 
 
 
